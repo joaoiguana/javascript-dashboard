6c8e1886336bba3d327ba050ef46dffc6929b33d
joao garcia <jiboiadamor@gmail.com>
2022-09-21 17:32:35 +0100

    dashboard 3 fetch api and dom maniputalion

--- js-dashboard-boilerplate-master/dashboard-3-assets/js/main.js
+++ js-dashboard-boilerplate-master/dashboard-3-assets/js/main.js
@@ -1,12 +1,69 @@
 // Load Dark Mode Settings
 
 // Get Colors From CSS Variables
+const getColorVariable = (color) =>
+  getComputedStyle(document.documentElement)
+    .getPropertyValue(`--color-${color}`)
+    .trim();
+
+const colorPrimary = getColorVariable("primary"),
+  colorAccent = getColorVariable("accent"),
+  colorDefault = getColorVariable("default"),
+  colorCard = getColorVariable("card"),
+  colorBorder = getColorVariable("border"),
+  colorGrey = getColorVariable("grey"),
+  colorLabel = getColorVariable("label");
+
+// Create Jquery style selector function
+const $ = (selector) => document.querySelector(selector);
+
+// Abbreviate long number function
+const abbreviateLongNumber = n => {
+  if (n < 1e3) return n;
+  if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(2) + "K";
+  if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
+  if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
+  if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
+};
 
 // Get Data From API
+const category = "beverages"
 
-// Get total uk products
+const getWorldProducts = async () => {
+  try {
+    const res = await fetch(
+      `https://world.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=${category}&json=true&sort_by=additives_n&page_size=6`
+    );
+    const data = await res.json();
 
-// Get drinks
+    const { count } = data;
+
+    $("#totalProducts").innerHTML = abbreviateLongNumber(count);
+    $("#preloader").classList.remove("drinks-loading");
+  } catch(error) {
+    console.log("error", error);
+  }
+};
+
+getWorldProducts();
+
+const getPTProducts = async () => {
+  try {
+    const res = await fetch(
+      `https://pt.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=${category}&json=true&sort_by=additives_n&page_size=6`
+    );
+    const data = await res.json();
+
+    const { count } = data;
+
+    $("#totalAdditives").innerHTML = abbreviateLongNumber(count);
+    $("#preloader").classList.remove("drinks-loading");
+  } catch(error) {
+    console.log("error", error);
+  }
+};
+
+getPTProducts();
 
 // Bar Figure Chart
 
